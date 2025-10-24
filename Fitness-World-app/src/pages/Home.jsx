199fd63a7ff053962f5dcc909fffd6980d86b04f
Emidio Pedro <emidiop44@gmail.com>
2025-10-24 10:35:19 +0200

    typo

--- Fitness-World-app/src/pages/Home.jsx
+++ Fitness-World-app/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import style from "../pages/Home.module.css";
-import BackgroundImg from "../assets/backgroundImg.png";
+import BackgroundImg from "../assets/BackgroundImg.png";
 import { Link } from "react-router-dom";
 
 export default function Home() {
